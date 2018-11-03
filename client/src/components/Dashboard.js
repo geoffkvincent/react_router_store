7dab34ce2db2525aa3cfe237c633cc9ccafdf631
Geoff Vincent <geoffkvincent@gmail.com>
2018-11-03 14:41:57 -0600

    dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -3,7 +3,11 @@ import {Redirect} from 'react-router-dom'
 import {isAuthenticated} from '../fakeAuth'
 
 const Dashboard = () => {
-
+  if (isAuthenticated()) {
+    return <h3>You are logged in</h3>
+  } else {
+    return <Redirect to='/login' />
+  }
 }
 
 export default Dashboard
