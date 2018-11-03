07e4a346bed6ea357e4f50f2bdf586a4d8a67a5f
Geoff Vincent <geoffkvincent@gmail.com>
2018-11-03 13:50:36 -0600

    add switch

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
-import React, {Fragment} from 'react'
-import {Route} from 'react-router-dom'
+import React from 'react'
+import {Route, Switch} from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import NoMatch from './components/NoMatch'
 
 const App = () => (
-  <Fragment>
+  <Switch>
     <Route exact path="/" component={Home} />
     <Route exact path="/about" component={About} />
     <Route component={NoMatch} />
-  </Fragment>
+  </Switch>
 )
 
 export default App
