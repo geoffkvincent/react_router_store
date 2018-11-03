9eb59381b8c364bb4ea620bcfaf87d66f1bffd84
Geoff Vincent <geoffkvincent@gmail.com>
2018-11-03 13:17:25 -0600

    controller

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -2,24 +2,36 @@ class Api::ProductsController < ApplicationController
   before_action :set_product, only: [:show, :update, :destroy]
 
   def index
-    
+    render json: Product.all
   end
 
   def show
-    
+    render json: @product
   end
 
   def create
-   
+    product = Product.new(product_name)
+
+    if product.save
+      render json: product
+    else
+      render json: product.errors, status: 422
+    end
   end
 
   def update
-    
+    if @product.update(product_params)
+      render json: @product
+    else
+      render json: @product.errors, status: 422
+    end
   end
 
   def destroy
-   
+    @product.destroy
   end
+
+  private
 
 
 end
