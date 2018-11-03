582f3b655845ef6b87444563daa438b1d616d06a
Geoff Vincent <geoffkvincent@gmail.com>
2018-11-03 13:49:16 -0600

    nomatch

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import {Route} from 'react-router-dom'
 import Home from './components/Home'
+import About from './components/About'
+import NoMatch from './components/NoMatch'
 
 const App = () => (
-  <Route exact path="/" component={Home} />
+  <Fragment>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/about" component={About} />
+    <Route component={NoMatch} />
+  </Fragment>
 )
 
 export default App
---
+++ client/src/components/NoMatch.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const NoMatch = ({ location }) => (
+  <h1>Page Not Found {location.pathname}</h1>
+)
+
+export default NoMatch
