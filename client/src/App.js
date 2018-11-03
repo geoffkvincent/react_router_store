ad83bf78705ded77b467f8d7ac05c190d8d97a51
Geoff Vincent <geoffkvincent@gmail.com>
2018-11-03 14:04:06 -0600

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import {Route, Switch} from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import NoMatch from './components/NoMatch'
+import NavBar from './components/NavBar'
 
 const App = () => (
-  <Switch>
-    <Route exact path="/" component={Home} />
-    <Route exact path="/about" component={About} />
-    <Route component={NoMatch} />
-  </Switch>
+  <Fragment>
+    <NavBar />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route component={NoMatch} />
+    </Switch>
+  </Fragment>
 )
 
 export default App
