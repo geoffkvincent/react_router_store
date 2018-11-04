ed6a39060d89ff6b47a1a86a6e82da4028d98207
Geoff Vincent <geoffkvincent@gmail.com>
2018-11-04 16:44:47 -0700

    products render

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,23 +1,29 @@
 import React from 'react'
-import {Redirect} from 'react-router-dom'
+import {Redirect, Link} from 'react-router-dom'
 import {isAuthenticated} from '../fakeAuth'
 
 class Dashboard extends React.Component {
-  render() {
+
+  render () {
     const {products} = this.state
-    if (isAuthenticated()){
-    return (
-      <ul>
-        {products.map(p =>
-          <li key={p.id}>
-            <Link to={`/products/${p.id}>{p.name}`}>
-              {p.name}
-            </Link>
-          </li>
-        )}
-      </ul>
-    )
+    if (isAuthenticated()) {
+      return (
+        <ul>
+          { products.map(p => 
+              <li key={p.id}>
+                <Link to={`/product/${p.id}`}>
+                  {p.name}
+                </Link>
+              </li>
+            )
+          }
+        </ul>
+      )
+    } else {
+      return <Redirect to="/login" />
+    }
   }
+}
 
 export default Dashboard
 
