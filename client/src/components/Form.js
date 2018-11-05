3cf4f183a7eb830f680f84350deb68b4cd6ea85d
Geoff Vincent <geoffkvincent@gmail.com>
2018-11-05 11:02:38 -0700

    form input

---
+++ client/src/components/Form.js
@@ -0,0 +1,53 @@
+import React from 'react'
+
+class Form extends React.Component {
+  dafaultValues = {name: '', price: '', description: '', department: ''}
+  state={...this.defaultValues}
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  handleChange = (e) => {
+    const {target: {name, value}} = e
+    this.setState({ [name]: value })
+  }
+
+  render () {
+    const { name, price, description, department } = this.state
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input 
+          name="name"
+          placeholder="Name"
+          value={name}
+          onChange={this.handleChange}
+          required
+        />
+        <input 
+          name="description"
+          placeholder="Description"
+          value={description}
+          onChange={this.handleChange}
+          required
+        />
+        <input 
+          name="price"
+          placeholder="Price"
+          value={price}
+          onChange={this.handleChange}
+          required
+        />
+        <input 
+          name="department"
+          placeholder="Department"
+          value={department}
+          onChange={this.handleChange}
+          required
+        />
+      </form>
+    )
+  }
+}
+
+export default Form
